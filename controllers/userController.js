714b6cae266ff5cb3da326644b6776b4232e433b
Arjunppp <arjunmohanak@gmail.com>
2024-07-29 18:39:00 +0530

    user page created

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,5 +3,15 @@
 
 export async function handleGetUserPage(req , res)
 {
-    res.send('OK');
+    try {
+        const logedUser = req.user;
+        //for the user --- find the projects in which he is memeber and the task which is associated
+        //Aggreagate users -projects - tasks
+        
+       
+        res.render('userPage')
+        
+    } catch (error) {
+        
+    }
 }
